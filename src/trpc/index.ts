995c4b2644605a49b69ce2457ae545399ce0c2e8
fred <fredjuma8@gmail.com>
2023-10-22 00:01:53 +0300

    onto database

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,19 @@
-import { router } from './trpc'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { publicProcedure, router } from './trpc'
+import { TRPCError } from '@trpc/server';
 
-const appRouter = router({
-    // ...
-});
+export const appRouter = router({
+    authCallback: publicProcedure.query(() => {
+        const {getUser} = getKindeServerSession()
+        const user = getUser()
 
-// Export type of router
-// NOT the router itself
+        if (!user.id || !user.email)
+            throw new TRPCError({ code: 'UNAUTHORIZED'})
+
+            // check if user is in database
+        
+        return { success: true }
+    }),
+})
+
 export type AppRouter = typeof appRouter;
