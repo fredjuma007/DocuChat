e3997a7223c72d6a40b892e490dc2ca5d5cd3653
fred <fredjuma8@gmail.com>
2023-11-29 02:09:30 +0300

    fixed the link issue, it was the import

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,8 +2,9 @@
 
 import { trpc } from "@/app/_trpc/client"
 import UploadButton from "./UploadButton"
-import { Ghost, Link } from "lucide-react"
+import { Ghost } from "lucide-react"
 import Skeleton from "react-loading-skeleton"
+import Link from 'next/link'
 
 const Dashboard = () => {
 const {data: files, isLoading} = trpc.getUserFiles.useQuery()
@@ -35,7 +36,7 @@ return <main className="mx-auto max-w-7xl md:p-10">
                   href={`/dashboard/${file.id}`}
                   className='flex flex-col gap-2'>
                   <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
-                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
+                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-red-500 to-orange-500' />
                     <div className='flex-1 truncate'>
                       <div className='flex items-center space-x-3'>
                         <h3 className='truncate text-lg font-medium text-zinc-900'>
