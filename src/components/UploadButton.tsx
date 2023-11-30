7f9f3234a1c24467e348e691b16c17d3a58d7ce5
fred <fredjuma8@gmail.com>
2023-11-30 20:36:20 +0300

    settiing up uploads

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -5,11 +5,46 @@ import { useState } from "react"
 import { Button } from "./ui/button"
 
 import Dropzone from "react-dropzone"
-import { Cloud } from "lucide-react"
+import { Cloud, File} from "lucide-react"
+import { Progress } from "./ui/progress"
+import { clear } from "console"
 
 const UploadDropzone = () => {
-    return <Dropzone multiple={false} onDrop={(acceptedFile) => {
-        console.log(acceptedFile)
+
+    const [isUploading, setIsUploading] = useState<boolean>(true)
+    const [uploadProgress, setUploadProgress] = useState<number>(0)
+
+    const startSimulatedProgress = () => {
+        setUploadProgress(0)
+
+        const interval = setInterval(() => {
+            setUploadProgress((prevProgress) => {
+                if(prevProgress >= 95) {
+                    clearInterval(interval)
+                    return prevProgress
+                }
+                return prevProgress + 5
+            })
+        }, 500)
+
+        return interval
+
+    }
+
+    return ( 
+    <Dropzone 
+        multiple={false} 
+        onDrop={async (acceptedFile) => {
+        setIsUploading(true)
+
+        const progressInterval = startSimulatedProgress()
+
+        // simulate upload
+        //await new Promise((resolve) => setTimeout(resolve, 2000))
+
+        clearInterval(progressInterval)
+        setUploadProgress(100)
+
     }}>
         {({getRootProps, getInputProps, acceptedFiles}) => (
          <div
@@ -28,13 +63,30 @@ const UploadDropzone = () => {
                         or drag and drop a PDF
                     </p>
                     <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
-                    </div> 
+                    </div>
+
+                    {acceptedFiles && acceptedFiles[0] ? (
+                        <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-zinc-200 divid-x divide-zinc-200">
+                            <div className="px-3 py-2 h-full grid place-items-center">
+                                <File className="h-4 w-4 text-orange-500" />
+                            </div>
+                            <div className="px-3 py-2 h-full text-sm truncate">
+                                {acceptedFiles[0].name}
+                            </div>
+                        </div>
+                    ) : null}
+
+                    {isUploading ? (
+                        <div className="w-full mt-4 max-w-xs mx-auto">
+                            <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200"/>
+                        </div>
+                    ) : null}
                 </label>
             </div>
             </div>
         )}
     </Dropzone>
-}
+)}
 
 const UploadButton = () => {
 const [isOpen, setIsOpen] = useState<boolean>(false)
